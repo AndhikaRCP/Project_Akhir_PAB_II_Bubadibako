70a23ecc042ad60b367ce3fd9049208e35ea5946
AndhikaRCP <andhikarizky2603@gmail.com>
2024-05-28 11:31:13 +0700

    make screen structure

---
+++ lib/screens/follower_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FollowerScreen extends StatefulWidget {
+  const FollowerScreen({super.key});
+
+  @override
+  State<FollowerScreen> createState() => _FollowerScreenState();
+}
+
+class _FollowerScreenState extends State<FollowerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
