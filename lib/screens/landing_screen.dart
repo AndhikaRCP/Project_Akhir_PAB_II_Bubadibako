9e84e5b2cd2c3a268ef3c84c4af816d4cc161d5f
williamtanuwijaya <will.tw0807@gmail.com>
2024-05-28 12:11:06 +0700

    fix landing screen

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -10,16 +10,53 @@ class LandingScreen extends StatefulWidget {
 class _LandingScreenState extends State<LandingScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
+            Image.asset('assets/image/logo.png'),
+            const SizedBox(height: 32.0),
+            const Text(
               'Bubadibako',
-              textAlign: TextAlign.start,
-            )
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'LuckiestGuy',
+                fontSize: 32.0,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+                shadows: [
+                  Shadow(
+                    offset: Offset(3.0, 3.0),
+                    blurRadius: 0.0,
+                    color: Colors.black,
+                  ),
+                  Shadow(
+                    offset: Offset(-3.0, -3.0),
+                    blurRadius: 0.0,
+                    color: Colors.black,
+                  ),
+                  Shadow(
+                    offset: Offset(3.0, -3.0),
+                    blurRadius: 0.0,
+                    color: Colors.black,
+                  ),
+                  Shadow(
+                    offset: Offset(-3.0, 3.0),
+                    blurRadius: 0.0,
+                    color: Colors.black,
+                  ),
+                  Shadow(
+                    offset: Offset(0.0, 0.0),
+                    blurRadius: 0.0,
+                    color: Colors.white,
+                  ),
+                ],
+                letterSpacing: 7.0,
+              ),
+            ),
           ],
         ),
       ),
