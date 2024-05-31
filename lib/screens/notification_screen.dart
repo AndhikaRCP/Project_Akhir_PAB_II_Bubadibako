19df0b7fd989217b96c631637fb90239866f0b7b
PeterReynardSusanto <Peterreynard20@gmail.com>
2024-05-31 09:05:32 +0700

    update Notification

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -1,55 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:project_akhir_pab_ii_bubadibako/data/list_notification.dart';
+import 'package:project_akhir_pab_ii_bubadibako/screens/detailnotifikasi_screen.dart';
 
-class NotificationScreen extends StatefulWidget {
+class ListNotification extends StatefulWidget {
+  const ListNotification({Key? key}) : super(key: key);
+
   @override
-  _NotificationScreenState createState() => _NotificationScreenState();
+  _ListNotificationState createState() => _ListNotificationState();
 }
 
-class _NotificationScreenState extends State<NotificationScreen> {
-  final List<NotificationItem> notifications = [
-    NotificationItem(
-        'Richie Pakusalam', 'comment your post', 'about 5 hours ago'),
-    NotificationItem('Zaky Jonathan Simanjuntak', 'comment likes your post',
-        'about 17 hours ago'),
-    NotificationItem('Andhika Tanuwijaya Sitohang', 'comment likes your post',
-        'about 1 day ago'),
-    NotificationItem(
-        'Cristopher Rajaguguk', 'comment your post', 'about 2 days ago'),
-    NotificationItem(
-        'Williskuy Wala Wala', 'comment your post', 'about 3 days ago'),
-  ];
-
+class _ListNotificationState extends State<ListNotification> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Notification'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.notifications),
-            onPressed: () {},
-          ),
-        ],
+        title: const Text('Notifikasi'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
       body: ListView.builder(
-        itemCount: notifications.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            leading: Icon(Icons.account_circle, size: 40),
-            title: Text(notifications[index].user),
-            subtitle: Text(notifications[index].action),
-            trailing: Text(notifications[index].timeElapsed),
+          final notification = listNotification[index];
+
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      DetailNotifikasi(notification: notification),
+                ),
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              decoration: const BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(color: Colors.black38, width: 1)),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    notification.judul,
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    notification.deskripsiNotifikasi,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
           );
         },
+        itemCount: listNotification.length,
       ),
     );
   }
-}
-
-class NotificationItem {
-  final String user;
-  final String action;
-  final String timeElapsed;
-
-  NotificationItem(this.user, this.action, this.timeElapsed);
 }
