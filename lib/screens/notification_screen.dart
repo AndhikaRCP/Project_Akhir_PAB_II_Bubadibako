49785a34649ccb367a339c7926c716ac8bb6bf4f
PeterReynardSusanto <Peterreynard20@gmail.com>
2024-05-31 08:41:49 +0700

    add notification_screen

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
-
+class NotificationScreen extends StatefulWidget {
   @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
+  _NotificationScreenState createState() => _NotificationScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen> {
+class _NotificationScreenState extends State<NotificationScreen> {
+  final List<NotificationItem> notifications = [
+    NotificationItem(
+        'Richie Pakusalam', 'comment your post', 'about 5 hours ago'),
+    NotificationItem('Zaky Jonathan Simanjuntak', 'comment likes your post',
+        'about 17 hours ago'),
+    NotificationItem('Andhika Tanuwijaya Sitohang', 'comment likes your post',
+        'about 1 day ago'),
+    NotificationItem(
+        'Cristopher Rajaguguk', 'comment your post', 'about 2 days ago'),
+    NotificationItem(
+        'Williskuy Wala Wala', 'comment your post', 'about 3 days ago'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notification'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: notifications.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.account_circle, size: 40),
+            title: Text(notifications[index].user),
+            subtitle: Text(notifications[index].action),
+            trailing: Text(notifications[index].timeElapsed),
+          );
+        },
+      ),
+    );
   }
+}
+
+class NotificationItem {
+  final String user;
+  final String action;
+  final String timeElapsed;
+
+  NotificationItem(this.user, this.action, this.timeElapsed);
 }
