2e752ba228a15ad00bfe4ca0f7f9d2c9563e8708
Richie Jonathan Chaniago <richiejonathanchaniago@gmail.com>
2024-06-01 19:38:57 +0700

    Posting Screen Fix image from file

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
-    import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:project_akhir_pab_ii_bubadibako/dashboard_screen.dart';
 import 'package:project_akhir_pab_ii_bubadibako/screens/favorite_screen.dart';
+import 'package:project_akhir_pab_ii_bubadibako/screens/posting_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-    class MyApp extends StatelessWidget {
-      const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +18,7 @@ void main() {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: FavoritesScreen(),
+      home: PostingScreen(),
     );
   }
 }
--- lib/screens/posting_screen.dart
+++ lib/screens/posting_screen.dart
@@ -1,129 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
-// import 'dart:io';
-
-// class PostingScreen extends StatefulWidget {
-//   @override
-//   _PostingScreenState createState() => _PostingScreenState();
-// }
-
-// class _PostingScreenState extends State<PostingScreen> {
-//   final _captionController = TextEditingController();
-//   List<File> _galleryImages = [];
-
-//   @override
-//   void dispose() {
-//     _captionController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Center(child: Text('New Post')),
-//           leading: IconButton(
-//             icon: Icon(Icons.close),
-//             onPressed: () {
-//               // Handle close button press
-//               Navigator.pop(context);
-//             },
-//           ),
-//           actions: [
-//             IconButton(
-//               icon: Icon(Icons.check),
-//               onPressed: () {
-//                 // Handle posting
-//                 _handlePost();
-//               },
-//             ),
-//           ],
-//         ),
-//         body: SingleChildScrollView(
-//           child: Column(
-//             children: [
-//               // Display the selected gallery image in a grid
-//               GridView.builder(
-//                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//                   crossAxisCount:
-//                       1, // Set crossAxisCount to 1 to display a single image
-//                   crossAxisSpacing: 1.5,
-//                   mainAxisSpacing: 1.5,
-//                   childAspectRatio: 1.0,
-//                 ),
-//                 shrinkWrap: true,
-//                 physics: NeverScrollableScrollPhysics(),
-//                 itemCount:
-//                     _galleryImages.length > 0 ? 1 : 0, // Only display one image
-//                 itemBuilder: (context, index) {
-//                   return Image.file(
-//                       _galleryImages.last); // Display the last selected image
-//                 },
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.all(16.0),
-//                 child: TextField(
-//                   controller: _captionController,
-//                   decoration: InputDecoration(
-//                     hintText: 'Masukkan caption anda...',
-//                     border: OutlineInputBorder(),
-//                   ),
-//                 ),
-//               ),
-
-//               AppBar(
-//                 title: Center(child: Text('Gallery Anda')),
-//                 actions: [
-//                   IconButton(
-//                     icon: Icon(Icons.camera),
-//                     onPressed: () {
-//                       // Handle picking image
-//                       _handleGallery();
-//                     },
-//                   ),
-//                 ],
-//               ),
-
-//               GridView.builder(
-//                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//                   crossAxisCount: 2,
-//                   crossAxisSpacing: 1.5,
-//                   mainAxisSpacing: 1.5,
-//                   childAspectRatio: 1.0,
-//                 ),
-//                 shrinkWrap: true,
-//                 physics: NeverScrollableScrollPhysics(),
-//                 itemCount: _galleryImages.length,
-//                 itemBuilder: (context, index) {
-//                   return Image.file(_galleryImages[index]);
-//                 },
-//               )
-//             ],
-//           ),
-//         ));
-//   }
-
-//   void _handlePost() {
-//     // Handle posting logic here
-//     print('Posting...');
-//     print('Caption: ${_captionController.text}');
-//     print('Images: ${_galleryImages.length}');
-//   }
-
-//   void _handleGallery() async {
-//     // Handle gallery selection logic here
-//     print('Selecting from gallery...');
-//     final ImagePicker _picker = ImagePicker();
-//     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-//     if (image != null) {
-//       setState(() {
-//         _galleryImages.add(File(image.path));
-//       });
-//     }
-//   }
-// }
-
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
@@ -131,13 +5,15 @@ import 'dart:io';
 import 'package:project_akhir_pab_ii_bubadibako/models/post.dart';
 
 class PostingScreen extends StatefulWidget {
+  const PostingScreen({super.key});
+
   @override
   _PostingScreenState createState() => _PostingScreenState();
 }
 
 class _PostingScreenState extends State<PostingScreen> {
   final _captionController = TextEditingController();
-  List<Posting> _postList = [];
+  final List<Posting> _postList = [];
 
   void _showImageDialog(String imageUrl) {
     showDialog(
@@ -151,12 +27,9 @@ class _PostingScreenState extends State<PostingScreen> {
             ),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(6.0),
-              child: CachedNetworkImage(
-                imageUrl: imageUrl,
-                placeholder: (context, url) => const Center(
-                  child: CircularProgressIndicator(),
-                ),
-                errorWidget: (context, url, error) => const Icon(Icons.error),
+              child: Image.file(
+                File(imageUrl),
+                errorBuilder: (context, url, error) => const Icon(Icons.error),
                 fit: BoxFit.cover,
               ),
             ),
@@ -176,9 +49,9 @@ class _PostingScreenState extends State<PostingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('New Post')),
+        title: const Center(child: Text('New Post')),
         leading: IconButton(
-          icon: Icon(Icons.close),
+          icon: const Icon(Icons.close),
           onPressed: () {
             // Handle close button press
             Navigator.pop(context);
@@ -186,7 +59,7 @@ class _PostingScreenState extends State<PostingScreen> {
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.check),
+            icon: const Icon(Icons.check),
             onPressed: () {
               // Handle posting
               _handlePost();
@@ -198,54 +71,39 @@ class _PostingScreenState extends State<PostingScreen> {
         child: Column(
           children: [
             // Display the selected gallery image in a grid
-            GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount:
-                    1, // Set crossAxisCount to 1 to display a single image
-                crossAxisSpacing: 1.5,
-                mainAxisSpacing: 1.5,
-                childAspectRatio: 1.0,
-              ),
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: _postList?.isNotEmpty == true
-                  ? 1
-                  : 0, // Display only one image if list is not empty
-              itemBuilder: (context, index) {
-                return _postList?.isNotEmpty == true
-                    ? GestureDetector(
-                        onTap: () =>
-                            _showImageDialog(_postList.last.imageAsset),
-                        child: ClipRRect(
-                          borderRadius:
-                              BorderRadius.circular(10), // Add a rounded corner
-                          child: Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black, width: 1),
-                            ),
-                            child: Image.network(_postList.last.imageAsset,
-                                fit: BoxFit.cover),
-                          ),
+            _postList.isNotEmpty
+                ? GestureDetector(
+                    onTap: () => _showImageDialog(_postList.last.imageAsset),
+                    child: ClipRRect(
+                      borderRadius:
+                          BorderRadius.circular(10), // Add a rounded corner
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 1),
                         ),
-                      )
-                    : Container(); // Return an empty container if list is empty
-              },
-            ),
+                        child: Image.file(
+                          File(_postList.last.imageAsset),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  )
+                : const SizedBox.shrink(),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
                 controller: _captionController,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   hintText: 'Masukkan caption anda...',
                   border: OutlineInputBorder(),
                 ),
               ),
             ),
             AppBar(
-              title: Center(child: Text('Gallery Anda')),
+              title: const Center(child: Text('Gallery Anda')),
               actions: [
                 IconButton(
-                  icon: Icon(Icons.camera),
+                  icon: const Icon(Icons.camera),
                   onPressed: () {
                     // Handle picking image
                     _handleGallery();
@@ -254,14 +112,14 @@ class _PostingScreenState extends State<PostingScreen> {
               ],
             ),
             GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 crossAxisSpacing: 1.5,
                 mainAxisSpacing: 1.5,
                 childAspectRatio: 1.0,
               ),
               shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
+              physics: const NeverScrollableScrollPhysics(),
               itemCount: _postList.length,
               itemBuilder: (context, index) {
                 final post = _postList.reversed.toList()[index];
@@ -275,23 +133,15 @@ class _PostingScreenState extends State<PostingScreen> {
                     ),
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(6.0),
-                      child: CachedNetworkImage(
-                        imageUrl: post.imageAsset,
-                        placeholder: (context, url) => const Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                        errorWidget: (context, url, error) =>
+                      child: Image.file(
+                        File(post.imageAsset),
+                        errorBuilder: (context, file, error) =>
                             const Icon(Icons.error),
                         fit: BoxFit.cover,
                       ),
                     ),
                   ),
                 );
-                // return Container(
-                //     decoration: BoxDecoration(
-                //       border: Border.all(color: Colors.black, width: 1),
-                //     ),
-                //     child: Image.network(_postList[index].imageAsset));
               },
             ),
           ],
