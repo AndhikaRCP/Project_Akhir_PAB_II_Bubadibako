05131bd71b974141d93f8be0a51ddee4ec6cfc55
Richie Jonathan Chaniago <richiejonathanchaniago@gmail.com>
2024-06-03 22:26:02 +0700

    darktheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,21 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-    import 'package:project_akhir_pab_ii_bubadibako/screens/activity_screen.dart';
-    import 'package:project_akhir_pab_ii_bubadibako/screens/detail_screen.dart';
+import 'package:project_akhir_pab_ii_bubadibako/screens/activity_screen.dart';
+import 'package:project_akhir_pab_ii_bubadibako/screens/detail_screen.dart';
 import 'package:project_akhir_pab_ii_bubadibako/screens/edit_profile_screen.dart';
-    import 'package:project_akhir_pab_ii_bubadibako/screens/favorite_screen.dart';
-    import 'package:project_akhir_pab_ii_bubadibako/screens/follower_screen.dart';
-    import 'package:project_akhir_pab_ii_bubadibako/screens/following_screen.dart';
-    import 'package:project_akhir_pab_ii_bubadibako/screens/landing_screen.dart';
-    import 'package:project_akhir_pab_ii_bubadibako/screens/notification_screen.dart';
-    import 'package:project_akhir_pab_ii_bubadibako/screens/profile_screen.dart';
-    // import 'package:project_akhir_pab_ii_bubadibako/screens/search_screen.dart';
-    import 'package:project_akhir_pab_ii_bubadibako/screens/sign_in_screen.dart';
-    import 'package:project_akhir_pab_ii_bubadibako/screens/sign_up_screen.dart';
+import 'package:project_akhir_pab_ii_bubadibako/screens/favorite_screen.dart';
+import 'package:project_akhir_pab_ii_bubadibako/screens/follower_screen.dart';
+import 'package:project_akhir_pab_ii_bubadibako/screens/following_screen.dart';
+import 'package:project_akhir_pab_ii_bubadibako/screens/landing_screen.dart';
+import 'package:project_akhir_pab_ii_bubadibako/screens/notification_screen.dart';
+import 'package:project_akhir_pab_ii_bubadibako/screens/profile_screen.dart';
+// import 'package:project_akhir_pab_ii_bubadibako/screens/search_screen.dart';
+import 'package:project_akhir_pab_ii_bubadibako/screens/sign_in_screen.dart';
+import 'package:project_akhir_pab_ii_bubadibako/screens/sign_up_screen.dart';
 import 'package:project_akhir_pab_ii_bubadibako/widgets/BottomNavBarWidget.dart';
-    
-    
 
- void main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
@@ -25,32 +24,33 @@ import 'package:project_akhir_pab_ii_bubadibako/widgets/BottomNavBarWidget.dart'
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-      @override
-      Widget build(BuildContext context) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Bubadibako',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
-          home: LandingScreen(),
-          routes: {
-            '/activity': (context) => ActivityScreen(),
-            '/detail': (context) => const DetailScreen(),
-            '/favorite': (context) => FavoriteScreen(),
-            '/follower': (context) => const FollowerScreen(),
-            '/following': (context) => const FollowingScreen(),
-            '/landing': (context) =>  LandingScreen(),
-            '/notification': (context) => const NotificationScreen(),
-            '/bottomNav' :(context) =>  const BottomNavBarWidget(),
-            '/editProfile' :(context) =>   EditProfileScreen(),
-            '/profile': (context) => const ProfileScreen(),
-            // '/search': (context) => const SearchScreen(),
-            '/signIn': (context) => SignInScreen(),
-            '/signUp': (context) => SignUpScreen(),
-          },
-        );
-      }
-    }
-    
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveTheme(
+      light: ThemeData.light(useMaterial3: true),
+      dark: ThemeData.dark(useMaterial3: true),
+      initial: AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Bubadibako',
+        theme: theme,
+        home: LandingScreen(),
+        routes: {
+          '/activity': (context) => ActivityScreen(),
+          '/detail': (context) => const DetailScreen(),
+          '/favorite': (context) => FavoriteScreen(),
+          '/follower': (context) => const FollowerScreen(),
+          '/following': (context) => const FollowingScreen(),
+          '/landing': (context) => LandingScreen(),
+          '/notification': (context) => const NotificationScreen(),
+          '/bottomNav': (context) => const BottomNavBarWidget(),
+          '/editProfile': (context) => EditProfileScreen(),
+          '/profile': (context) => const ProfileScreen(),
+          // '/search': (context) => const SearchScreen(),
+          '/signIn': (context) => SignInScreen(),
+          '/signUp': (context) => SignUpScreen(),
+        },
+      ),
+    );
+  }
+}
--- lib/widgets/app_bar_widget.dart
+++ lib/widgets/app_bar_widget.dart
@@ -1,30 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 
-class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
+class AppBarWidget extends StatefulWidget implements PreferredSizeWidget {
   const AppBarWidget({Key? key, required this.title}) : super(key: key);
   final String title;
+
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
 
   @override
+  _AppBarWidgetState createState() => _AppBarWidgetState();
+}
+
+class _AppBarWidgetState extends State<AppBarWidget> {
+  @override
   Widget build(BuildContext context) {
     return AppBar(
-      title:  Text(this.title),
+      title: Text(widget.title),
       backgroundColor: Colors.grey,
-      automaticallyImplyLeading: false, // Menghilangkan tombol back arrow
+      automaticallyImplyLeading: false,
       actions: [
+         Switch.adaptive(
+          value: AdaptiveTheme.of(context).mode.isDark,
+          onChanged: (state) {
+            AdaptiveTheme.of(context).toggleThemeMode(useSystem: false);
+          },
+          activeTrackColor: Colors.grey[300],
+
+        ),
         Container(
           margin: const EdgeInsets.all(10.0),
           child: ElevatedButton(
             onPressed: () {
               FirebaseAuth.instance.signOut().whenComplete(() {
+
+                //////back to white again
+                AdaptiveTheme.of(context).reset(); //
                 Navigator.of(context).pushReplacementNamed("/signIn");
               });
             },
             child: const Text("Logout"),
           ),
         ),
+       
       ],
     );
   }
